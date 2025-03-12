264a1684fc80df5def946e0f30a56d05eceaff3c
jananpatel2002 <jananpatel2002@gmail.com>
2025-03-12 11:43:11 -0400

    changed name and deploying after done

--- src/portfolio.js
+++ src/portfolio.js
@@ -20,10 +20,10 @@ const illustration = {
 };
 
 const greeting = {
-  username: "Saad Pasta",
-  title: "Hi all, I'm Saad",
+  username: "Janan Patel",
+  title: "Hi all, I'm Janan",
   subTitle: emoji(
-    "A passionate Full Stack Software Developer 🚀 having an experience of building Web and Mobile applications with JavaScript / Reactjs / Nodejs / React Native and some other cool libraries and frameworks."
+    "A passionate Full Stack Software Developer 🚀 specializing in building web applications with React, Vue.js, TypeScript, and Node.js. Experienced in both frontend and backend development with a strong focus on creating scalable and user-friendly solutions."
   ),
   resumeLink:
     "https://drive.google.com/file/d/1ofFdKF_mqscH8WvXkSObnVvC9kK7Ldlu/view?usp=sharing", // Set to empty to hide the button
@@ -33,16 +33,11 @@ const greeting = {
 // Social Media Links
 
 const socialMediaLinks = {
-  github: "https://github.com/saadpasta",
-  linkedin: "https://www.linkedin.com/in/saadpasta/",
-  gmail: "saadpasta70@gmail.com",
-  gitlab: "https://gitlab.com/saadpasta",
-  facebook: "https://www.facebook.com/saad.pasta7",
-  medium: "https://medium.com/@saadpasta",
-  stackoverflow: "https://stackoverflow.com/users/10422806/saad-pasta",
-  // Instagram, Twitter and Kaggle are also supported in the links!
-  // To customize icons and social links, tweak src/components/SocialMedia
-  display: true // Set true to display this section, defaults to false
+  github: "https://github.com/jananpatel2002",
+  linkedin: "https://www.linkedin.com/in/janan-patel-99408923b/",
+  gmail: "jananpatel2002@gmail.com",
+  // Remove or comment out unused social media links
+  display: true
 };
 
 // Skills Section
